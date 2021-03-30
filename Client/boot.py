196468334eef4ac70fcb0dab892333d67bca3dcc
ICheered <tjeerd992@gmail.com>
2021-03-30 16:12:56 +0200

    OTA

--- Client/boot.py
+++ Client/boot.py
@@ -1,7 +1,62 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 import esp
-esp.osdebug(None)
-import uos, machine
 import gc
+import machine
+import network
+import upip
 
-gc.collect()
+from config import wifi_ssid, wifi_pass
+
+
+def connect_wlan(ssid, password):
+    """Connects build-in WLAN interface to the network.
+    Args:
+        ssid: Service name of Wi-Fi network.
+        password: Password for that Wi-Fi network.
+    Returns:
+        True for success, Exception otherwise.
+    """
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    sta_if.active(True)
+    ap_if.active(False)
+
+    if not sta_if.isconnected():
+        print("Connecting to WLAN ({})...".format(ssid))
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            pass
+
+    return True
+
+
+def main():
+    """Main function. Runs after board boot, before main.py
+    Connects to Wi-Fi and checks for latest OTA version.
+    """
+    esp.osdebug(None)
+
+    gc.collect()
+    gc.enable()
+
+    # Wi-Fi credentials
+    SSID = wifi_ssid
+    PASSWORD = wifi_pass
+
+    connect_wlan(SSID, PASSWORD)
+
+    # Install Senko from PyPi
+    upip.install("micropython-senko")
+
+    import senko
+    from config import GITHUB_URL
+    OTA = senko.Senko(url=GITHUB_URL, files=["boot.py", "devices,py", "main.py"])
+
+    if OTA.update():
+        print("Updated to the latest version! Rebooting...")
+        machine.reset()
+
+
+if __name__ == "__main__":
+    main()
