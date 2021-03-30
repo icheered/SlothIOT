f5ae979edd03a7c5a07b19ffade8018bb54005c5
ICheered <tjeerd992@gmail.com>
2021-03-30 17:19:20 +0200

    f

--- Client/boot.py
+++ Client/boot.py
@@ -49,6 +49,8 @@ def main():
     from config import GITHUB_USER, GITHUB_REPO, GITHUB_DIR, GITHUB_BRANCH
     OTA = senko.Senko(user=GITHUB_USER, repo=GITHUB_REPO, working_dir=GITHUB_DIR, branch=GITHUB_BRANCH, files=["main.py", "boot.py", "devices.py"])
 
+    print("Checking for update")
+
     if OTA.update():
         print("Updated to the latest version! Rebooting...")
         machine.reset()
